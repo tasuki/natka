29cc492eda38252ea78c8e0833a6702facdfecf8
Vit Brunner <vit.brunner@gmail.com>
2020-09-05 22:22:53 +0200

    Add prev/next arrows

--- public/scripts.js
+++ public/scripts.js
@@ -1,13 +1,37 @@
 $(document).ready(function() {
-  img = $("#project-image img");
-  spinner = $("#spinner");
-  $("#project-thumbs a").on("click", function(e) {
-    e.preventDefault();
-    href = $(this).attr("href");
+  var img = $("#project-image img");
+  var spinner = $("#spinner");
+  var images = $("#project-thumbs a").map(function(link) {
+    return $(this).attr('href');
+  }).toArray()
+  var imageCount = images.length
+
+  function switchImage(href) {
     spinner.show();
     img.attr("src", href);
     img.on("load", function() {
       spinner.hide();
     });
+  }
+
+  function shiftImage(shiftBy) {
+    var current = images.indexOf(img.attr("src"));
+    var newIndex = (imageCount + current + shiftBy) % imageCount;
+    switchImage(images[newIndex]);
+  }
+
+  $("#project-thumbs a").on("click", function(e) {
+    e.preventDefault();
+    switchImage($(this).attr("href"));
   });
+
+  $("#show-prev").on("click", function(e) {
+    e.preventDefault();
+    shiftImage(-1);
+  })
+
+  $("#show-next").on("click", function(e) {
+    e.preventDefault();
+    shiftImage(+1);
+  })
 });
